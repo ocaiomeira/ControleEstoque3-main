5ee9ecc9819184af95cee2246fdf05535617e68b
Caio Rian <107116396+ocaiomeira@users.noreply.github.com>
2022-07-21 16:40:27 -0300

    Update ProdutoPostRequestBody.java

--- src/main/java/com/projetoEstoque/ControleEstoque/requests/ProdutoPostRequestBody.java
+++ src/main/java/com/projetoEstoque/ControleEstoque/requests/ProdutoPostRequestBody.java
@@ -10,5 +10,8 @@ public class ProdutoPostRequestBody {
     @NotEmpty(message="O nome não pode estar vazio")
     @NotNull(message="O nome não pode estar nulo")
     private String name;
+    private String descricao;
+    @NotEmpty(message="O valor do produto não pode estar vazio")
+    private double preco;
 
 }
